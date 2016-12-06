ac5ba5c351c55091702d07eca97c97bcc378af5a
udelblue <cdsommers@gmail.com>
2016-12-06 07:55:40 -0500

    update service

--- src/main/java/moai/service/HistoryService.java
+++ src/main/java/moai/service/HistoryService.java
@@ -1,7 +1,10 @@
 package moai.service;
 
 import moai.domain.ItemMeta;
+import moai.domain.base.BaseStage;
 import moai.repository.HistoryRepository;
+
+import java.util.ArrayList;
 
 /**
  * Created by csommers on 12/5/2016.
@@ -18,7 +21,7 @@ public class HistoryService {
     }
 
 
-    public void write (ItemMeta meta , String event){
+    public void write (BaseStage basestage , ItemMeta meta , String event , String Comment, String usernameProformingAction, ArrayList<String> usernameActionProformedAgainst){
 
 
     }
