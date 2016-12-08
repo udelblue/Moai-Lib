1d758e2904cc94dd95f0aef4653750ad6d7b4b87
udelblue <cdsommers@gmail.com>
2016-12-08 09:39:57 -0500

    annotation added

--- src/main/java/moai/domain/Flow.java
+++ src/main/java/moai/domain/Flow.java
@@ -1,11 +1,82 @@
 package moai.domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 /**
  * Created by csommers on 11/18/2016.
  */
 public class Flow {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    int  id;
 
     String name;
     String description;
@@ -15,12 +86,6 @@ public class Flow {
     String created_by;
     String data;
 
-    public String getDescription() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
